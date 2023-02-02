6d64a5dfee4793790e952194f27f4d41e514e73b
Aleixbs <aleixbatlle@gmail.com>
2023-02-02 19:27:59 +0100

    Ejercicios JavaScript

---
+++ HTML/javascript/ejercicio5.js
@@ -0,0 +1,11 @@
+const meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+
+const mesesLargos = meses
+    .filter(meses => meses.length > 7)
+    .map(meses => meses.toUpperCase()); 
+
+console.log(mesesLargos)
+
+const numMesesLargos = mesesLargos.length; 
+
+console.log(`Hay ${numMesesLargos} meses largos`)
