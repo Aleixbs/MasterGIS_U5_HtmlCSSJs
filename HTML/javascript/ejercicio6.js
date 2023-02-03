0e74376a83a2aed85d2f721898a8b1ed3323fe0b
Aleixbs <aleixbatlle@gmail.com>
2023-02-03 09:17:25 +0100

    Ejercicios javascript

--- HTML/javascript/ejercicio6.js
+++ HTML/javascript/ejercicio6.js
@@ -5,5 +5,5 @@ console.log(nombre[0].toUpperCase() + nombre.substring(1));
 const empresa = 'environmental systems research institute'; 
 const listaEmpresa = empresa.split(" "); 
 for (let i = 0; i<listaEmpresa.length; i++) {
-    console.log(listaEmpresa[i][0].toUpperCase + listaEmpresa[i].substring(1));
+    console.log(listaEmpresa[i][0].toUpperCase() + listaEmpresa[i].substring(1));
 };
