72869813ac5b25f775586e884b58e5d3aa50170e
Aleixbs <aleixbatlle@gmail.com>
2023-02-07 16:57:47 +0100

    letraDNI

--- JavaScript/letraDNI/letraDNI.js
+++ JavaScript/letraDNI/letraDNI.js
@@ -2,20 +2,18 @@
 
 const letrasDNI = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E','T'];
 let numeroDNI = prompt("Por favor introduce el número de tu DNI, recuerda son 8 cifras","99999999");
-var resultadoDNI = 0;
-var letraDNI = letrasDNI[resultadoDNI];
 
-if (numeroDNI < 0 || numeroDNI > 99999999) {
+if (numeroDNI < 0 || numeroDNI > 99999999 || numeroDNI.length != 8) {
     alert("El número especificado no es válido");
-}   else if (numeroDNI.length !=  8) {
-    alert("El número especificado no es válido");
+// }   else if (numeroDNI.length !=  8) {
+//     alert("1El número especificado no es válido");
 }   else {
     var resultadoDNI = numeroDNI%23;
     var letraDNI = letrasDNI[resultadoDNI]
-    console.log(resultadoDNI)
+    // console.log(resultadoDNI)
+    // console.log(letraDNI)
+    alert(letraDNI)
+    // console.log(`El DNI completo es ${numeroDNI}${letraDNI}`)
+    alert(`El DNI completo es ${numeroDNI}${letraDNI}`)
 }
 
-console.log(letraDNI)
-alert(letraDNI)
-console.log(`El DNI completo es ${numeroDNI}${letraDNI}`)
-alert(`El DNI completo es ${numeroDNI}${letraDNI}`)
