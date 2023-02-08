b7ee1f505b3ff0fb57a6fcd0a9146d3497fcf546
Aleixbs <aleixbatlle@gmail.com>
2023-02-08 17:35:19 +0100

    Ejercicio de Crear Clases en JavaScript

---
+++ JavaScript/CrearClases/perimetroArea.js
@@ -0,0 +1,18 @@
+class square {
+    constructor(side){
+        this.perimeter = side*4;
+        this.area = side*side;
+        // this.showside = function(side) {
+        //     console.log(this.side)
+        // };
+    };
+};
+var pequeno = new square(2); 
+console.log(pequeno); 
+var mediano = new square(5); 
+var grande = new square(10); 
+
+function squareProperties(perimeter,area){
+    return console.log(pequeño.permietro);
+}
+console.log(pequeno.perimeter);
